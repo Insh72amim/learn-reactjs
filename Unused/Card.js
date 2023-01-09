5b80566c643084a99ab49b89e3d5dbfeaaeacc67
Insh72amim <42885018+Insh72amim@users.noreply.github.com>
2023-01-09 22:51:42 +0530

    Initial commit

---
+++ Unused/Card.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function Card() {
+  return (
+    <div className="Card">
+      <img src="https://reactjs.org/logo-og.png" alt="" />
+    </div>
+  );
+}
---
+++ src/Button/Button.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function Button() {
+  return (
+    <div className="Button">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button type="button" class="btn btn-primary btn-lg">
+        Large button
+      </button>
+      <button type="button" class="btn btn-secondary btn-lg">
+        Large button
+      </button>
+    </div>
+  );
+}
